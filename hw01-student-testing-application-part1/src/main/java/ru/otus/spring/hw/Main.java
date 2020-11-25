f74f2cfae8974ee1a897dd8da8c5c16b1f55ee63
Andrey <zhegalovan@mail.ru>
2020-11-25 05:48:39 -0500

    added hw01 struct

---
+++ hw01-student-testing-application-part1/src/main/java/ru/otus/spring/hw/Main.java
@@ -0,0 +1,11 @@
+package ru.otus.spring.hw;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("/spring-context.xml");
+        context.close();
+    }
+}
