dd531ae1acd52c814d4952287179d9535f57204c
Andrey <zhegalovan@mail.ru>
2020-11-30 22:49:33 +0300

    added lombok dependencies

--- hw01-student-testing-application-part1/src/main/java/ru/otus/spring/hw/domain/Answer.java
+++ hw01-student-testing-application-part1/src/main/java/ru/otus/spring/hw/domain/Answer.java
@@ -1,40 +1,14 @@
 package ru.otus.spring.hw.domain;
 
-import java.util.Objects;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
 
+@Getter
+@ToString
+@EqualsAndHashCode
+@RequiredArgsConstructor
 public class Answer {
     private final String text;
-
-    public Answer(String text) {
-        this.text = text;
-    }
-
-    public String getText() {
-        return text;
-    }
-
-    @Override
-    public int hashCode() {
-        int result = 17;
-        result = 31 * result + (text != null ? text.hashCode() : 0);
-        return result;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o)
-            return true;
-        if (o == null || getClass() != o.getClass())
-            return false;
-
-        Answer object = (Answer) o;
-
-        return Objects.equals(text, object.text);
-    }
-
-    @Override
-    public String toString() {
-        return "Answer{" + "text = " + getText() + "}";
-    }
-
 }
--- hw01-student-testing-application-part1/src/main/java/ru/otus/spring/hw/domain/Question.java
+++ hw01-student-testing-application-part1/src/main/java/ru/otus/spring/hw/domain/Question.java
@@ -1,58 +1,16 @@
 package ru.otus.spring.hw.domain;
 
-import java.util.Objects;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
 
+@Getter
+@ToString
+@EqualsAndHashCode
+@RequiredArgsConstructor
 public class Question {
     private final int number;
     private final String text;
     private final Answer answer;
-
-    public Question(int number, String text, Answer answer) {
-        this.number = number;
-        this.text = text;
-        this.answer = answer;
-    }
-
-    public int getNumber() {
-        return number;
-    }
-
-    public String getText() {
-        return text;
-    }
-
-    public Answer getAnswer() {
-        return answer;
-    }
-
-    @Override
-    public int hashCode() {
-        int result = 17;
-        result = 31 * result + number;
-        result = 31 * result + (text != null ? text.hashCode() : 0);
-        result = 31 * result + (answer != null ? answer.hashCode() : 0);
-        return result;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o)
-            return true;
-        if (o == null || getClass() != o.getClass())
-            return false;
-
-        Question object = (Question) o;
-
-        if (number != object.number)
-            return false;
-        if (!Objects.equals(text, object.text))
-            return false;
-        return Objects.equals(answer, object.answer);
-    }
-
-    @Override
-    public String toString() {
-        return "Question{" + "number = " + getNumber() + ", text = " + getText() + ", answer = " + getAnswer() + "}";
-    }
-
 }
