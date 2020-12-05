cc459ee115a94c4403b9d8af0ab44bf8c6516e2e
Andrey <zhegalovan@mail.ru>
2020-12-05 17:11:44 +0300

    setup localization

--- hw03-student-testing-application-part3/src/main/java/ru/otus/spring/hw/Main.java
+++ hw03-student-testing-application-part3/src/main/java/ru/otus/spring/hw/Main.java
@@ -4,10 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
+import ru.otus.spring.hw.config.AppProps;
 import ru.otus.spring.hw.config.DataSourceConfig;
 import ru.otus.spring.hw.service.QuizService;
 
-@EnableConfigurationProperties(DataSourceConfig.class )
+@EnableConfigurationProperties({ DataSourceConfig.class, AppProps.class })
 @SpringBootApplication
 public class Main {
 
---
+++ hw03-student-testing-application-part3/src/main/java/ru/otus/spring/hw/config/AppProps.java
@@ -0,0 +1,16 @@
+package ru.otus.spring.hw.config;
+
+import java.util.Locale;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@ConfigurationProperties(prefix="application")
+public class AppProps {
+    private Locale locale;
+}
+
