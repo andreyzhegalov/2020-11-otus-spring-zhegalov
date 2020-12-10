5e4bdc1d24d1fef11bbed999c08dc014044ee536
Andrey <zhegalovan@mail.ru>
2020-12-10 22:14:01 +0300

    added localization to the data config

--- hw03-student-testing-application-part3/src/main/java/ru/otus/spring/hw/config/DataSourceConfig.java
+++ hw03-student-testing-application-part3/src/main/java/ru/otus/spring/hw/config/DataSourceConfig.java
@@ -1,13 +1,22 @@
 package ru.otus.spring.hw.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
-import lombok.Getter;
 import lombok.Setter;
 
-@Getter
 @Setter
 @ConfigurationProperties(prefix = "dataset")
 public class DataSourceConfig {
     private String filename;
+
+    @Autowired
+    private AppProps props;
+
+    public String getFilename() {
+        final var fileName = filename.substring(0, filename.lastIndexOf("."));
+        final var fileExtension = filename.substring(filename.lastIndexOf("."), filename.length());
+        final var localeSuffix = props.getLocale().toString();
+        return String.format("%s_%s%s", fileName, localeSuffix, fileExtension);
+    }
 }
--- hw03-student-testing-application-part3/src/main/java/ru/otus/spring/hw/config/ServiceConfig.java
+++ hw03-student-testing-application-part3/src/main/java/ru/otus/spring/hw/config/ServiceConfig.java
@@ -1,7 +1,5 @@
 package ru.otus.spring.hw.config;
 
-import java.util.Locale;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -15,15 +13,7 @@ public class ServiceConfig {
 
     @Bean
     QuestionDao questionDao(DataSourceConfig dataConfig, AppProps appProps) {
-        final var fileNameWithLocale = getFileNameWithLocale(dataConfig.getFilename(), appProps.getLocale());
-        return new CsvQuestionDao(fileNameWithLocale);
-    }
-
-    private String getFileNameWithLocale(String initFileName, Locale locale) {
-        final var fileName = initFileName.substring(0, initFileName.lastIndexOf("."));
-        final var fileExtension = initFileName.substring(initFileName.lastIndexOf("."), initFileName.length());
-        final var localeSuffix = locale.toString();
-        return String.format("%s_%s%s", fileName, localeSuffix, fileExtension);
+        return new CsvQuestionDao(dataConfig.getFilename());
     }
 
     @Bean
