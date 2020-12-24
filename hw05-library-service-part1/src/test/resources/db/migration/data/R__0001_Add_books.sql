e9ec72efbb21402997643051a4e5145331b22447
Andrey <zhegalovan@mail.ru>
2020-12-24 22:48:54 +0300

    added separate test data

--- hw05-library-service-part1/src/test/resources/db/migration/data/R__0001_Add_books.sql
+++ hw05-library-service-part1/src/test/resources/db/migration/data/R__0001_Add_books.sql
@@ -1,4 +1,3 @@
 insert into books(id, title)
 values (1, 'book1'),
-       (2, 'book2'),
-       (3, 'book3')
+       (2, 'book2')
---
+++ hw05-library-service-part1/src/test/resources/db/migration/data/R__0003_Add_reference_from_book_to_author.sql
@@ -0,0 +1,2 @@
+update books set author_id=2 where id=1;
+update books set author_id=1 where id=2
