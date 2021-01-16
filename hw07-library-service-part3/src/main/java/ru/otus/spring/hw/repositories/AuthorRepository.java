906d94b85b03af8663dcebc8e2f20e7b1aaa7864
Andrey <zhegalovan@mail.ru>
2021-01-16 13:01:14 +0300

    updated rpeositories

--- hw07-library-service-part3/src/main/java/ru/otus/spring/hw/repositories/AuthorRepository.java
+++ hw07-library-service-part3/src/main/java/ru/otus/spring/hw/repositories/AuthorRepository.java
@@ -1,8 +1,15 @@
 package ru.otus.spring.hw.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 import ru.otus.spring.hw.model.Author;
 
 public interface AuthorRepository extends JpaRepository<Author, Long> {
+
+    @Transactional(readOnly = true)
+    List<Author> findAll();
+
 }
--- hw07-library-service-part3/src/main/java/ru/otus/spring/hw/repositories/GenreRepository.java
+++ hw07-library-service-part3/src/main/java/ru/otus/spring/hw/repositories/GenreRepository.java
@@ -1,10 +1,16 @@
 package ru.otus.spring.hw.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import ru.otus.spring.hw.model.Genre;
 
 @Repository
 public interface GenreRepository extends JpaRepository<Genre, Long> {
+
+    @Transactional(readOnly = true)
+    List<Genre> findAll();
 }
