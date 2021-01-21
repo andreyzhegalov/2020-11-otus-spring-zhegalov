4ccc217c1e0dfd9168101b636d8f60e6b08db219
Andrey <zhegalovan@mail.ru>
2021-01-21 22:17:30 +0300

    moved comment repository to mongo

--- hw08-library-service-part4/src/main/java/ru/otus/spring/hw/model/Comment.java
+++ hw08-library-service-part4/src/main/java/ru/otus/spring/hw/model/Comment.java
@@ -1,14 +1,19 @@
 package ru.otus.spring.hw.model;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+@Document
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
 public class Comment {
-    private long id;
+    @Id
+    private String id;
 
     private String text;
 
--- hw08-library-service-part4/src/main/java/ru/otus/spring/hw/repositories/CommentRepository.java
+++ hw08-library-service-part4/src/main/java/ru/otus/spring/hw/repositories/CommentRepository.java
@@ -1,21 +1,15 @@
 package ru.otus.spring.hw.repositories;
 
 import java.util.List;
-import java.util.Optional;
 
+import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 import ru.otus.spring.hw.dto.CommentDto;
 import ru.otus.spring.hw.model.Comment;
 
 @Repository
-public interface CommentRepository {
+public interface CommentRepository extends MongoRepository<Comment, String> {
 
     List<CommentDto> findAllBy();
-
-    Optional<Comment> findById(Long id);
-
-    void save(Comment comment);
-
-    void deleteById(Long id);
 }
