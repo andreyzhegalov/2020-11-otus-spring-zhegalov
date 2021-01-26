efd82916a6ceffb49c63f9beec89fb779ef0ed47
Andrey <zhegalovan@mail.ru>
2021-01-26 17:43:42 +0300

    remove unused code

--- hw08-library-service-part4/src/main/java/ru/otus/spring/hw/service/IOAuthorService.java
+++ hw08-library-service-part4/src/main/java/ru/otus/spring/hw/service/IOAuthorService.java
@@ -6,22 +6,14 @@ import java.util.stream.Collectors;
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
-import ru.otus.spring.hw.dto.AuthorDto;
 import ru.otus.spring.hw.model.Author;
 import ru.otus.spring.hw.model.Book;
 
 @RequiredArgsConstructor
 @Service
 public class IOAuthorService {
-    private static final String GET_AUTHOR_ID = "Введите идентификатор автора:";
 
     private final IOService ioService;
-
-    public AuthorDto getAuthor() {
-        ioService.print(GET_AUTHOR_ID);
-        final var authorId = ioService.read();
-        return new AuthorDto(authorId);
-    }
 
     private void printAuthor(Author author) {
         final StringBuilder sb = new StringBuilder();
