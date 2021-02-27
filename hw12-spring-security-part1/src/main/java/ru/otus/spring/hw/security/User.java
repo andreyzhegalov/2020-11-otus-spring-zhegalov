4cb9b7ea62d5e69a4530685654293f64eeb93896
Andrey <zhegalovan@mail.ru>
2021-02-27 13:50:04 +0300

    added users to db

---
+++ hw12-spring-security-part1/src/main/java/ru/otus/spring/hw/security/User.java
@@ -0,0 +1,26 @@
+package ru.otus.spring.hw.security;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Document(value = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    private String id;
+
+    private String name;
+
+    private String password;
+
+    public User(String name, String password) {
+        this.name = name;
+        this.password = password;
+    }
+}
