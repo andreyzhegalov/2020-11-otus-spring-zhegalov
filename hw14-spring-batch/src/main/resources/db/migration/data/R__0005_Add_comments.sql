9871fe91cd7cd0b1e88194fbc1efaa92ad07d530
Andrey <zhegalovan@mail.ru>
2021-03-15 22:11:49 +0300

    updated db migration script

--- hw14-spring-batch/src/main/resources/db/migration/data/R__0005_Add_comments.sql
+++
@@ -1,5 +0,0 @@
-insert into comments(id, text, book_id)
-values (1, 'comment1', 1),
-       (2, 'comment2', 1),
-       (3, 'comment3', 3);
-
--- hw14-spring-batch/src/test/resources/db/migration/data/R__0005_Add_comments.sql
+++
@@ -1,4 +0,0 @@
-insert into comments(id, text, book_id)
-values (1, 'comment1', 1),
-       (2, 'comment2', 2);
-
