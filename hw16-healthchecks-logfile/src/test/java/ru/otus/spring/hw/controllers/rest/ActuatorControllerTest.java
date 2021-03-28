0c81603f51bd2f73f566b2ecd2fb81f13d4047f7
Andrey <zhegalovan@mail.ru>
2021-03-28 14:19:52 +0300

    updated actuator config

--- hw16-healthchecks-logfile/src/test/java/ru/otus/spring/hw/controllers/rest/ActuatorControllerTest.java
+++ hw16-healthchecks-logfile/src/test/java/ru/otus/spring/hw/controllers/rest/ActuatorControllerTest.java
@@ -1,22 +1,10 @@
 package ru.otus.spring.hw.controllers.rest;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.hamcrest.Matchers.hasItem;
-import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.notNullValue;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.BDDMockito.given;
-import static org.mockito.BDDMockito.then;
-import static org.mockito.Mockito.doThrow;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.jupiter.api.Test;
@@ -41,12 +29,6 @@ public class ActuatorControllerTest {
     @Test
     void shouldReturnBodyWithLinksForActuatorRequest() throws Exception {
         mvc.perform(get("/actuator")).andDo(print()).andExpect(status().isOk())
-                .andExpect(jsonPath("$._links", is(notNullValue())));
-    }
-
-    @Test
-    void shouldReturnMetricsInTheForRequest() throws Exception{
-        mvc.perform(get("/actuator/metrics")).andDo(print()).andExpect(status().isOk())
                 .andExpect(jsonPath("$._links", is(notNullValue())));
     }
 }
